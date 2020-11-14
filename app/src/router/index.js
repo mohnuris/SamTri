95f511ac95693a0b89ad2295fc63fd9994801c3b
mohnuris <nuris.ft.unuja@gmail.com>
2020-11-15 01:29:48 +0700

    JsonServeDb

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,11 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHashHistory
+} from 'vue-router'
 import Home from '../views/Home.vue'
+import Users from '../views/Users.vue'
 
-const routes = [
-  {
+
+const routes = [{
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/Users',
+    name: 'Users',
+    comments: Users
+
   },
   {
     path: '/about',
@@ -13,12 +23,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
