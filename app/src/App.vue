0eb9bb128af95e7c1c1542bcef76e4e9a755affc
nuris.ft.unuja@gmail.com <nuris.ft.unuja@gmail.com>
2020-12-26 05:06:10 +0700

    Add components

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>|
+  <div id="app">
+    <!-- <router-link to="/">Home</router-link>|
     <router-link to="/users">Users</router-link>|
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link>-->
     <router-view />
+    <layout />
   </div>
 </template>
+
+<script>
+import Layout from "../../../api/src/components/Layout.vue";
+// import Layout from "./components/Layout.vue";
+
+export default {
+  name: "app",
+  components: {
+    Layout,
+    // Layout,
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ app/src/components/TabNav.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="width: 30%;">
+    <ul class="nav nav-tabs">
+      <li v-for="tab in tabs" :key="tab" class="nav-item">
+        <a class="nav-link" :class="{ active: tab === selected }" @click="setTab(tab)">{{ tab }}</a>
+      </li>
+    </ul>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default {
+  props: {
+    tabs: {
+      type: Array,
+      required: true,
+    },
+    selected: {
+      type: String,
+      required: true,
+    },
+  },
+  methodName: {
+    setTab(tab) {
+      this.$emit("selected", tab);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
